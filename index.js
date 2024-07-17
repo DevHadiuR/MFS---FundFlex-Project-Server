180ddd68d6b5c2ea0372a7fee28fa8854bd77951
Hadiur Rahman <hadiurahman139@gmail.com>
2024-07-17 15:09:14 +0600

    create jwt token and make a a route for sending jwt to the  machine

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const jwt = require("jsonwebtoken");
 
 const app = express();
@@ -24,6 +24,45 @@ app.use(express.json());
 app.get("/", async (req, res) => {
   res.send("Funflex Server Site Activate!");
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASS}@cluster0.vuymtad.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // All Collections are here
+    const userCollection = client.db("fundFlexDB").collection("allUsers");
+
+    // jwt area
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.Access_Secret_token, {
+        expiresIn: "10d",
+      });
+      res.send({ token });
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log("Server is running on port : ", port);
